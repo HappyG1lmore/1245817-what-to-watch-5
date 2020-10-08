953eeade367b4c3310028994c5732404156bfd33
Андрей <cereperec@gmail.com>
2020-10-08 09:35:40 +0300

    добавлены компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import Main from "./../main/main";
 import PropTypes from "prop-types";
+import SignIn from "./../sign_in/sign_in";
+import MyList from "./../my_list/my_list";
+import Film from "./../film/film";
+import AddReview from "./../add_review/add_review";
+import Player from "./../player/player";
+
+
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 
 const App = (props) => {
   const mainFilm = props.mainFilm;
   return (
-    <Main
-      mainFilm={mainFilm}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main mainFilm={mainFilm} />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id?">
+          <Film />
+        </Route>
+        <Route exact path="/player/:id?">
+          <Player />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
